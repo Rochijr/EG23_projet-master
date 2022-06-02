6ee72cdcdea3e2e9e1357808ae4d33f226261337
Antoine <antoine.roche@utt.fr>
2022-06-02 12:59:36 +0200

    Page Redéploiement avec DnD.

--- app/src/main/java/com/duolight/projeteg23/controller/RedeploiementArmee.java
+++ app/src/main/java/com/duolight/projeteg23/controller/RedeploiementArmee.java
@@ -13,42 +13,40 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.Layout;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.duolight.projeteg23.R;
 
-public class DeploiementArmee extends AppCompatActivity implements View.OnClickListener {
-    private static final String SHARED_PREF_JOUEUR_1_INFO = "SHARED_PREF_JOUEUR_1_INFO"; // nom du fichier
-    private static final String SHARED_PREF_JOUEUR_1_INFO_KEY = "SHARED_PREF_JOUEUR_1_INFO_KEY"; // Key
-    private TextView mBranche;
-    private ImageView next;
-
+public class RedeploiementArmee extends AppCompatActivity {
     private static final String VIEW_TAG1 = "Chef de guerre";
     private static final String VIEW_TAG2 = "Soldat d'élite";
     private static final String VIEW_TAG3 = "Alpha";
     private static final String VIEW_TAG4 = "Bravo";
     private static final String VIEW_TAG5 = "Charlie";
 
+
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deploiement_armee);
-        mBranche=findViewById(R.id.joueur1);
-        String brancheJoueur1 = getSharedPreferences(SHARED_PREF_JOUEUR_1_INFO,MODE_PRIVATE).getString(SHARED_PREF_JOUEUR_1_INFO_KEY,null);
-        mBranche.setText("Joueur 1 : " + brancheJoueur1);
+        setContentView(R.layout.activity_redeploiement_armee);
 
-        next = findViewById(R.id.arrow_right);
-        next.setOnClickListener(this);
+        ImageView next = findViewById(R.id.arrow_right);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // fonction déclenchée sur le clic du bouton
+                // Création d’une activité associée à l’exécution de MaGestionListe.class
+                Intent intent = new Intent(RedeploiementArmee.this, FinDePartie.class);
+                // Exécution de l’activité : ouverture de la fenêtre
+                startActivity(intent);
+            }
+        });
 
         // Create new views to drags
         LinearLayout groupe1 = (LinearLayout) findViewById(R.id.armee_CdG);
@@ -74,35 +72,35 @@ public class DeploiementArmee extends AppCompatActivity implements View.OnClickL
         groupe1.setOnLongClickListener(v -> {
             ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
             ClipData dragData = new ClipData((CharSequence) v.getTag(), new String[] { ClipDescription.MIMETYPE_TEXT_PLAIN }, item);
-            View.DragShadowBuilder myShadow = new DeploiementArmee.MyDragShadowBuilder(groupe1);
+            View.DragShadowBuilder myShadow = new RedeploiementArmee.MyDragShadowBuilder(groupe1);
             v.startDragAndDrop(dragData, myShadow,null,0);
             return true;
         });
         groupe2.setOnLongClickListener(v -> {
             ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
             ClipData dragData = new ClipData((CharSequence) v.getTag(), new String[] { ClipDescription.MIMETYPE_TEXT_PLAIN }, item);
-            View.DragShadowBuilder myShadow = new DeploiementArmee.MyDragShadowBuilder(groupe1);
+            View.DragShadowBuilder myShadow = new RedeploiementArmee.MyDragShadowBuilder(groupe1);
             v.startDragAndDrop(dragData, myShadow,null,0);
             return true;
         });
         groupe3.setOnLongClickListener(v -> {
             ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
             ClipData dragData = new ClipData((CharSequence) v.getTag(), new String[] { ClipDescription.MIMETYPE_TEXT_PLAIN }, item);
-            View.DragShadowBuilder myShadow = new DeploiementArmee.MyDragShadowBuilder(groupe1);
+            View.DragShadowBuilder myShadow = new RedeploiementArmee.MyDragShadowBuilder(groupe1);
             v.startDragAndDrop(dragData, myShadow,null,0);
             return true;
         });
         groupe4.setOnLongClickListener(v -> {
             ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
             ClipData dragData = new ClipData((CharSequence) v.getTag(), new String[] { ClipDescription.MIMETYPE_TEXT_PLAIN }, item);
-            View.DragShadowBuilder myShadow = new DeploiementArmee.MyDragShadowBuilder(groupe1);
+            View.DragShadowBuilder myShadow = new RedeploiementArmee.MyDragShadowBuilder(groupe1);
             v.startDragAndDrop(dragData, myShadow,null,0);
             return true;
         });
         groupe5.setOnLongClickListener(v -> {
             ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
             ClipData dragData = new ClipData((CharSequence) v.getTag(), new String[] { ClipDescription.MIMETYPE_TEXT_PLAIN }, item);
-            View.DragShadowBuilder myShadow = new DeploiementArmee.MyDragShadowBuilder(groupe1);
+            View.DragShadowBuilder myShadow = new RedeploiementArmee.MyDragShadowBuilder(groupe1);
             v.startDragAndDrop(dragData, myShadow,null,0);
             return true;
         });
@@ -136,13 +134,10 @@ public class DeploiementArmee extends AppCompatActivity implements View.OnClickL
                     v.setBackgroundResource(R.drawable.border_reserve);
                     ImageView imageView = new ImageView(this);
 
-                    //LinearLayout linearLayout = (LinearLayout) findViewById(R.id.zone1);
                     LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                             LinearLayout.LayoutParams.WRAP_CONTENT,
                             LinearLayout.LayoutParams.WRAP_CONTENT
                     );
-                    //layoutParams.addRule(RelativeLayout.BELOW, R.id.ButtonRecalculate);
-                    //layoutParams. addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
 
                     if(dragData.equals(groupe1.getTag())) {
                         imageView.setImageResource(R.drawable.logo_chefdeguerre);
@@ -160,7 +155,6 @@ public class DeploiementArmee extends AppCompatActivity implements View.OnClickL
                         imageView.setImageResource(R.drawable.logo_charlie);
                         addSoldat(1, 4);
                     }
-                    //((LinearLayout)v).setLayoutParams(layoutParams);
                     ((LinearLayout)v).addView(imageView, layoutParams);
                     return true;
 
@@ -424,63 +418,27 @@ public class DeploiementArmee extends AppCompatActivity implements View.OnClickL
     }
 
     private static class MyDragShadowBuilder extends View.DragShadowBuilder {
-        // The drag shadow image, defined as a drawable object.
         private static Drawable shadow;
 
-        // Constructor
         public MyDragShadowBuilder(View v) {
-            // Stores the View parameter.
             super(v);
-            // Creates a draggable image that fills the Canvas provided by the system.
             shadow = new ColorDrawable(Color.LTGRAY);
         }
 
-        // Defines a callback that sends the drag shadow dimensions and touch point
-        // back to the system.
         @Override
         public void onProvideShadowMetrics (Point size, Point touch) {
-            // Defines local variables
             int width, height;
-            // Set the width of the shadow to half the width of the original View.
             width = getView().getWidth() / 2;
-            // Set the height of the shadow to half the height of the original View.
             height = getView().getHeight() / 2;
 
-            // The drag shadow is a ColorDrawable. This sets its dimensions to be the
-            // same as the Canvas that the system provides. As a result, the drag shadow
-            // fills the Canvas.
             shadow.setBounds(0, 0, width, height);
-
-            // Set the size parameter's width and height values. These get back to the
-            // system through the size parameter.
             size.set(width, height);
-
-            // Set the touch point's position to be in the middle of the drag shadow.
             touch.set(width / 2, height / 2);
         }
 
-        // Defines a callback that draws the drag shadow in a Canvas that the system
-        // constructs from the dimensions passed to onProvideShadowMetrics().
         @Override
         public void onDrawShadow (Canvas canvas) {
-            // Draw the ColorDrawable on the Canvas passed in from the system.
             shadow.draw(canvas);
         }
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == next) {
-            pageSuivante();
-        }
-        else {
-            throw new IllegalStateException("Unknown clicked view : " + v);
-        }
-    }
-    public void pageSuivante() {
-        // Création d’une activité associée à l’exécution de MaGestionListe.class
-        Intent intent = new Intent(DeploiementArmee.this, Combat.class);
-        // Exécution de l’activité : ouverture de la fenêtre
-        startActivity(intent);
     }
 }
